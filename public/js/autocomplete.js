3b84a52b12ac9ab14119ee99fac989f7d8431839
Irina Meroi <irimeroi7@gmail.com>
2023-12-06 21:39:03 -0800

    form styling and comments iteration

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -9,5 +9,3 @@ function initAutoComplete() {
         });
     });
 };
-
-initAutoComplete();
--- public/js/commentHandler.js
+++ public/js/commentHandler.js
@@ -1,9 +1,10 @@
 const commentFormHandler = async (event) => {
     event.preventDefault();
+    
+    const description = document.querySelector(`[data-description='${event.target.id}']`).value;
+    const post_id = event.target.id;
 
-    const description = document.querySelector('#comment-body').value.trim();
-    const post_id = document.querySelector('#post_id').value;
-
+    console.log(document.querySelector(`[data-description='${event.target.id}']`).value)
     console.table({ post_id, description });
 
     if (description && post_id) {
@@ -22,4 +23,8 @@ const commentFormHandler = async (event) => {
     }
 };
 
-document.querySelector('#comment-button').addEventListener('click', commentFormHandler);
+const commentButton = document.querySelectorAll('.comment-button');
+for (var i=0; i < commentButton.length; i++) {
+    commentButton[i].addEventListener('click', commentFormHandler);
+};
+
