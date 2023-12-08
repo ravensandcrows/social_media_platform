176eba63dfd2e6be799303f675d1e983fa3866d5
chriswaje <chriswaje324@gmail.com>
2023-12-07 17:31:02 -0800

    FIXED: autocomplete feature

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -1,11 +1,13 @@
-// function initAutoComplete() {
-//     const searchInput = document.querySelector('input[name="location"]');
-//     document.addEventListener('DOMContentLoaded', function() {
-//         const autocomplete = new google.maps.places.Autocomplete(searchInput, {
-//             types: ['geocode'],
-//         });
-//         autocomplete.addListener('place_changed', function () {
-//             const nearPlace = autocomplete.getPlace();
-//         });
-//     });
-// };
+function initAutoComplete() {
+    const searchInput = document.querySelector('input[name="location"]');
+    document.addEventListener('DOMContentLoaded', function() {
+        const autocomplete = new google.maps.places.Autocomplete(searchInput, {
+            types: ['geocode'],
+        });
+        autocomplete.addListener('place_changed', function () {
+            const nearPlace = autocomplete.getPlace();
+        });
+    });
+};
+
+initAutoComplete();
