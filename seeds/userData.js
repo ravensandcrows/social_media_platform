61af9ba3ebbd570bc81dffde3ebd1d3a24d47308
Irina Meroi <irimeroi7@gmail.com>
2023-12-07 18:25:00 -0800

    changed css for comment button

--- seeds/userData.js
+++ seeds/userData.js
@@ -2,22 +2,22 @@ const { User } = require('../models');
 
 const userData = [
     {
-        "username": "christian",
+        "username": "chrisw",
         "email": "christian@gmail.com",
         "password": "qwert12345"
     },
     {
-        "username": "jandrei",
+        "username": "jandreit",
         "email": "jandrei@gmail.com",
         "password": "12345qwert"
     },
     {
-        "username": "kali",
+        "username": "kalib",
         "email": "kali@hotmail.com",
         "password": "poiuy09876"
     },
     {
-        "username": "irina",
+        "username": "irim",
         "email": "irina@hotmail.com",
         "password": "09876poiuy"
     }
